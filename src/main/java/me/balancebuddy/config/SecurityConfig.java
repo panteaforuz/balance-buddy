6cc24f99088a683e1a680e719cd4e1e6896258e9
pantea foruz <panteaforuz@gmail.com>
2024-12-06 19:55:50 +0330

    added security config class

---
+++ src/main/java/me/balancebuddy/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package me.balancebuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated());
+//                .oauth2ResourceServer( oauth2 -> oauth2.jwt(jwt -> 10L));
+        return http.build();
+    }
+}
